21de65960ef7af0753ee3c9165581b49866021b4
Armando Ruiz <artmx@proton.me>
2025-02-11 12:05:24 -0600

    Update
    overflow scroll in the index carrousel component

--- src/components/index-carrousel/indexCarrousel.mjs
+++ src/components/index-carrousel/indexCarrousel.mjs
@@ -24,7 +24,7 @@ export class IndexCarrousel extends HTMLElement {
       <p class="index-carrousel-paragraph">I recently start a <strong>Frontend developer Roadmap</strong>  online, after a four months, finally I start my own personal site!</p>
       <p class="index-carrousel-paragraph">My stack: <strong>VanillaJS</strong>, <strong>HTML</strong>, <strong>CSS</strong>, <strong>Sass</strong> and <strong>Web Component Structure</strong></p>
       <p class="index-carrousel-paragraph">Pure, clean, and simple code. I like it. No frameworks... yet!</p>
-      <p class="index-carrousel-paragraph">My first component is a <strong>Dark-Light Toggle button</strong>. Check it out at the <strong>right superior corner</strong> of your screen.</p>
+      <p class="index-carrousel-paragraph">My first component is a <strong>Dark-Light Toggle button</strong>. Check it out at the <strong>right superior corner</strong> of your desktop/tablet screen or <strong>left inferior corner</strong> of your mobile screen.</p>
       </div>
     `
     return indexCarrousel;
